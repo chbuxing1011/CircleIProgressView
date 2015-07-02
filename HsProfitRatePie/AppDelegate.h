98de8fced8ebde71e90eec45bea9b8c1321126af
snailgame <snailgame@AllendeiMac.local>
2015-07-02 11:38:13 +0800

    Initial Commit

---
+++ HsProfitRatePie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HsProfitRatePie
+//
+//  Created by snailgame on 15/7/2.
+//  Copyright (c) 2015年 SnailGame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
